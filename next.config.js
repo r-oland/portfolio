408aee2c566a2ab2fd433098f9b12b4d75655e2d
Roland Branten <roland.branten@gmail.com>
2022-09-14 20:39:01 +0200

    Setup next.config for multiple domains

--- next.config.js
+++ next.config.js
@@ -7,5 +7,17 @@ module.exports = {
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: 'nl',
+    domains: [
+      {
+        domain: 'rolandbranten.com',
+        defaultLocale: 'en',
+        http: true,
+      },
+      {
+        domain: 'rolandbranten.nl',
+        defaultLocale: 'nl',
+        http: true,
+      },
+    ],
   },
 };
