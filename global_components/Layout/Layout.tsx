df5e77983c735f70bab1d5ae84af44ba720ba0e3
Roland Branten <roland.branten@gmail.com>
2022-09-22 19:12:31 +0200

    Fix overflow bug mobile devices

--- global_components/Layout/Layout.tsx
+++ global_components/Layout/Layout.tsx
@@ -4,20 +4,12 @@ import DesktopNav from './DesktopNav/DesktopNav';
 import MobileNav from './MobileNav/MobileNav';
 // =========================
 
-function Component({ children }: { children: JSX.Element }) {
-  return (
-    <>
-      <DesktopNav />
-      <MobileNav />
-      {children}
-    </>
-  );
-}
-
 export default function Layout({ children }: { children: JSX.Element }) {
   return (
     <LazyMotion features={domAnimation} strict>
-      <Component>{children}</Component>
+      <DesktopNav />
+      <MobileNav />
+      {children}
     </LazyMotion>
   );
 }
