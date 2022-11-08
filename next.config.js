526048892dbe0cd8371f2f6915df04d43e54d46c
Roland Branten <roland.branten@gmail.com>
2022-11-09 00:57:55 +0100

    i18n test locales test

--- next.config.js
+++ next.config.js
@@ -1,36 +1,20 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   i18n: {
-    // These are all the locales you want to support in
-    // your application
     locales: ['en', 'nl'],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
     defaultLocale: 'nl',
     domains: [
       {
         domain: 'rolandbranten.com',
         defaultLocale: 'en',
+        locales: ['en'],
       },
       {
         domain: 'rolandbranten.nl',
         defaultLocale: 'nl',
+        locales: ['nl'],
       },
     ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/en/:path*/',
-        destination: 'https://rolandbranten.com/:path*/',
-        locale: false,
-      },
-      {
-        source: '/nl/:path*/',
-        destination: 'https://rolandbranten.nl/:path*/',
-        locale: false,
-      },
-    ];
   },
   async redirects() {
     return [
